ee83f89e9e0ba90e464572e6f517bc6a74597d9d
Hadi T <hadi@syniol.com>
2023-11-04 16:09:41 +0000

    Added the package library

---
+++ sycrypt.go
@@ -0,0 +1,95 @@
+package sycrypt
+
+import (
+	"bytes"
+	"crypto/ed25519"
+	"crypto/rand"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/pem"
+	"fmt"
+)
+
+type Credential struct {
+	PublicKey      string `json:"publicKey"`
+	PrivateKey     string `json:"privateKey"`
+	HashedPassword string `json:"hashedPassword"`
+}
+
+func NewCredential(password string) (*Credential, error) {
+	public, private, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+
+	pubKeyBytes, err := x509.MarshalPKIXPublicKey(public)
+	if err != nil {
+		return nil, fmt.Errorf("unable to marshal public key: %s", err.Error())
+	}
+
+	var pemEncodedPublicKey bytes.Buffer
+	if err := pem.Encode(&pemEncodedPublicKey, &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: pubKeyBytes,
+	}); err != nil {
+		return nil, err
+	}
+
+	prvKeyBytes, err := x509.MarshalPKCS8PrivateKey(private)
+	if err != nil {
+		return nil, fmt.Errorf("unable to marshal private key: %s", err.Error())
+	}
+
+	var pemEncodedPrivateKey bytes.Buffer
+	if err := pem.Encode(&pemEncodedPrivateKey, &pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: prvKeyBytes,
+	}); err != nil {
+		return nil, err
+	}
+
+	return &Credential{
+		PublicKey:  encodeHash(pemEncodedPublicKey.Bytes()),
+		PrivateKey: encodeHash(pemEncodedPrivateKey.Bytes()),
+		HashedPassword: encodeHash(
+			ed25519.Sign(private, []byte(password)),
+		),
+	}, nil
+}
+
+func (cred *Credential) VerifyPassword(inputPassword string) bool {
+	return ed25519.Verify(
+		decodePublicCert(decodeHash([]byte(cred.PublicKey))),
+		[]byte(inputPassword),
+		decodeHash([]byte(cred.HashedPassword)),
+	)
+}
+
+func decodeHash(hash []byte) []byte {
+	base64Hash := make([]byte, base64.StdEncoding.DecodedLen(len(hash)))
+	base64.StdEncoding.Decode(base64Hash, hash)
+
+	hexHash := make([]byte, hex.DecodedLen(len(base64Hash)))
+	hex.Decode(hexHash, base64Hash)
+
+	return hexHash
+}
+
+func encodeHash(plaintext []byte) string {
+	hexHash := make([]byte, hex.EncodedLen(len(plaintext)))
+	hex.Encode(hexHash, plaintext)
+
+	base64Hash := make([]byte, base64.StdEncoding.EncodedLen(len(hexHash)))
+	base64.StdEncoding.Encode(base64Hash, hexHash)
+
+	return string(base64Hash)
+}
+
+func decodePublicCert(cert []byte) []byte {
+	out, _ := pem.Decode(cert)
+
+	parsedPublicKey, _ := x509.ParsePKIXPublicKey(out.Bytes)
+
+	return parsedPublicKey.(ed25519.PublicKey)
+}
---
+++ sycrypt_test.go
@@ -0,0 +1,62 @@
+package sycrypt
+
+import "testing"
+
+func TestNewCredential(t *testing.T) {
+	t.Run("Given password is correct", func(t *testing.T) {
+		actual, err := NewCredential("Johnspa$$word")
+		if err != nil {
+			t.Fatal("it was not expecting an error but got", err.Error())
+		}
+
+		isVerified := actual.VerifyPassword("Johnspa$$word")
+		if isVerified != true {
+			t.Error("it was expecting to be verified")
+		}
+	})
+
+	t.Run("Given password is incorrect", func(t *testing.T) {
+		actual, err := NewCredential("Johnspa$$word")
+		if err != nil {
+			t.Fatal("it was not expecting an error but got", err.Error())
+		}
+
+		isVerified := actual.VerifyPassword("Johnspa$$wordNotMatching")
+		if isVerified == true {
+			t.Error("it was expecting to not verify the incorrect password")
+		}
+	})
+}
+
+func TestCredentialVerifyPassword(t *testing.T) {
+	actual, _ := NewCredential("johnspassword1!")
+
+	t.Run("correct", func(t *testing.T) {
+		ok := actual.VerifyPassword("johnspassword1!")
+		if !ok {
+			t.Error("it should have verified the password")
+		}
+	})
+
+	t.Run("incorrect", func(t *testing.T) {
+		ok := actual.VerifyPassword("johnspassword1!IncorrecT")
+		if ok {
+			t.Error("it should have not verified the password")
+		}
+	})
+}
+
+func TestCredentialVerifyPasswordFromRawValues(t *testing.T) {
+	var sut *Credential
+
+	sut = &Credential{
+		PublicKey:      "MmQyZDJkMmQyZDQyNDU0NzQ5NGUyMDUwNTU0MjRjNDk0MzIwNGI0NTU5MmQyZDJkMmQyZDBhNGQ0MzZmNzc0MjUxNTk0NDRiMzI1Njc3NDE3OTQ1NDE2MTVhNTc2NjZlNmUzNzQxNmU0YzQ0NDY2MTRmMzM0NzMwNDQ2OTdhNzA1NzRhNDY2MjdhNzI3NjcyNDIzMTRhNjE3YTc1NzM0NjY5MzQ2ODY4Mzg2NzNkMGEyZDJkMmQyZDJkNDU0ZTQ0MjA1MDU1NDI0YzQ5NDMyMDRiNDU1OTJkMmQyZDJkMmQwYQ==",
+		PrivateKey:     "MmQyZDJkMmQyZDQyNDU0NzQ5NGUyMDUwNTI0OTU2NDE1NDQ1MjA0YjQ1NTkyZDJkMmQyZDJkMGE0ZDQzMzQ0MzQxNTE0MTc3NDI1MTU5NDQ0YjMyNTY3NzQyNDM0OTQ1NDk0ZTMzNDY2NTU3NzM0ZjMzNTg1MDM2MzkzMzZmMzk0ZTY0NDE0NDM5NDMzODMwMzM2YzY1Njk3MTZjMmI2MTU4NTk3ODU0NDM2MjQyNDg1MDJmNzQ2YjBhMmQyZDJkMmQyZDQ1NGU0NDIwNTA1MjQ5NTY0MTU0NDUyMDRiNDU1OTJkMmQyZDJkMmQwYQ==",
+		HashedPassword: "ZDRkM2QzMWQyN2JjZTYyZTRjODI2MTFkYmZjMzk0YmIzNTI4MmRhODMwYTBhMWI3NjBiZjhkZjQzOGZjZDViOTViMGI4ZDBjMTY5ZjlhMzAxNGIwMGY4ZDVlYTMyMWE5MDAzNzVhNGE0MWZhMTFhZDViNjEwYTg0YTk2ZTAyMDI=",
+	}
+
+	ok := sut.VerifyPassword("johnspassword1")
+	if !ok {
+		t.Error("password is not matching")
+	}
+}
